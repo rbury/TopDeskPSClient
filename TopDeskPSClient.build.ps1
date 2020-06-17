54c2f368a741692a0da74d3d270877950ff3c59d
Ray Bury <ray@raybury.com>
2020-06-17 12:40:58 -0400

    Update releasenotes path

--- TopDeskPSClient.build.ps1
+++ TopDeskPSClient.build.ps1
@@ -30,7 +30,7 @@ task regularBuild {
     $newversion = (($env:PROJECT_VERSION -split '-')[0]).Replace('v', '')
     Set-ModuleFunction -Name "$env:BuildOutput/$env:ProjectName.psd1" -FunctionsToExport $PublicFunctions
     Update-Metadata -Path "$env:BuildOutput/$env:ProjectName.psd1" -PropertyName ModuleVersion -Value $newversion
-    $ReleaseNotes = (Get-Content -Path "$env:ProjectName/ReleaseNotes.md") -replace '## v0.0.0','## v'+$newversion
+    $ReleaseNotes = (Get-Content -Path "./ReleaseNotes.md") -replace '## v0.0.0','## v'+$newversion
     Update-Metadata -Path "$env:BuildOutput/$env:ProjectName.psd1" -PropertyName ReleaseNotes -Value $ReleaseNotes
 }
 
